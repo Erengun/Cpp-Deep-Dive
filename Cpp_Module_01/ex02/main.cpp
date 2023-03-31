98f11f3dcc324867430374ba7414731f5fd03a56
Eren Gün <erengun4@gmail.com>
2023-03-31 16:02:11 +0300

    Bug and Makefile fixes Never gonna make you cry

--- Cpp_Module_01/ex02/main.cpp
+++ Cpp_Module_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: egun <egun@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 21:22:03 by egun              #+#    #+#             */
-/*   Updated: 2023/03/26 21:22:04 by egun             ###   ########.fr       */
+/*   Updated: 2023/03/31 15:29:24 by egun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,5 @@ int main( void )
     std::cout << &str << ": " << str << std::endl;
     std::cout << stringPTR << ": " << *stringPTR << std::endl;
     std::cout << &stringREF << ": " << stringREF << std::endl;
-
     return 0;
 }
